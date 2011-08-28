7a20bb98a80addf5fe637c1624e7aefa68150f8a
Brett Hardin <hardin.brett@gmail.com>
2011-08-28 13:27:27 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Infomang::Application.config.secret_token = '2cadce31f8858cc80da43f683c1e02b89846ed3782bbec7a237fd209366dd49c70e261e1cd53951826979c993416946022c873b40fa228e975dc235d9305efd8'
